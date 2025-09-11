3e33a30d319de70d3b07bc1cf6e1a8caf9ee3255
Mordon-YT <muradsnouber@icloud.com>
2025-09-11 12:28:39 +0300

    adding the template

---
+++ menuDB.js
@@ -0,0 +1,358 @@
+menu = {
+    hooka: [
+    {
+    name: "ليمون ونعنع",
+    price: 23,
+    img: "./imgs/hooka/lemon.jpg"
+    }, {
+    name: "بطيخ ونعنع",
+    price: 23,
+    img: "./imgs/hooka/watermelon.jpg"
+    }, {
+    name: "تفاختين",
+    price: 23,
+    img: "./imgs/hooka/apple.jpg"
+    }, {
+    name: "بلوبري",
+    price: 23,
+    img: "./imgs/hooka/blueberry.jpg"
+    }, {
+    name: "Love S1",
+    price: 23,
+    img: "./imgs/hooka/loveS1.jpg"
+    }, {
+    name: "كيف",
+    price: 23,
+    img: "./imgs/hooka/keef.jpg"
+    }, {
+    name: "سهم وقرفة",
+    price: 23,
+    img: "./imgs/hooka/arrow.jpg"
+    }, {
+    name: "تفاحتين نخلة",
+    price: 28,
+    img: "./imgs/hooka/palmApple.jpg"
+    }
+    ],
+    dessert: [
+        {
+            name: "San Sebastian",
+            price: 20,
+            img: "./imgs/dessert/sanSebastian.jpg"
+        }, {
+            name: "Soufflé",
+            price: 20,
+            img: "./imgs/dessert/souffle.jpg"
+        },
+    ],
+    iceCream: [
+        {
+            name: "Glass Serving",
+            price: 8,
+            img: "./imgs/iceCream/glass.jpg"
+        }, {
+            name: "Cone Serving",
+            price: 6,
+            img: "./imgs/iceCream/cone.jpg"
+        }, {
+            name: "American Ice Cream",
+            price: "5/8",
+            img: "./imgs/iceCream/americanIce.jpg"
+        },
+    ],
+    milkshake: [
+        {
+            name: "Strawberry",
+            price: 15,
+            img: "./imgs/milkshake/strawberry.jpg"
+        }, {
+            name: "Oreo",
+            price: 15,
+            img: "./imgs/milkshake/oreo.jpg"
+        }, {
+            name: "Vanilla",
+            price: 15,
+            img: "./imgs/milkshake/vanilla.jpg"
+        }, {
+            name: "Nutella",
+            price: 15,
+            img: "./imgs/milkshake/nutella.jpg"
+        }, {
+            name: "Pistachio",
+            price: 15,
+            img: "./imgs/milkshake/pistachio.jpg"
+        }, {
+            name: "Lotus",
+            price: 15,
+            img: "./imgs/milkshake/lotus.jpg"
+        }, {
+            name: "Blueberry",
+            price: 15,
+            img: "./imgs/milkshake/blueberry.jpg"
+        }, {
+            name: "Pampa",
+            price: 15,
+            img: "./imgs/milkshake/pampa.jpg"
+        }, {
+            name: "Cerelac",
+            price: 15,
+            img: "./imgs/milkshake/cerelac.jpg"
+        }, {
+            name: "Caramel",
+            price: 15,
+            img: "./imgs/milkshake/caramel.jpg"
+        }
+    ],
+    iceDrinks: [
+        {
+            name: "Ice Mocha",
+            price: 13,
+            img: "./imgs/iceDrinks/iceMocha.jpg"
+        }, {
+            name: "Ice Coffee",
+            price: 13,
+            img: "./imgs/iceDrinks/iceCoffee.jpg"
+        }, {
+            name: "Ice Spanish Latte",
+            price: 13,
+            img: "./imgs/iceDrinks/iceSpanishLatte.jpg"
+        }, {
+            name: "Ice Latte Vanilla",
+            price: 13,
+            img: "./imgs/iceDrinks/iceLatteVanilla.jpg"
+        }, {
+            name: "Ice Latte Hazelnut",
+            price: 13,
+            img: "./imgs/iceDrinks/iceLatteHazelnut.jpg"
+        }, {
+            name: "Ice Tea Peach",
+            price: 13,
+            img: "./imgs/iceDrinks/iceTeaPeach.jpg",
+        }, {
+            name: "Ice Tea Blueberry",
+            price: 13,
+            img: "./imgs/iceDrinks/iceTeaLemon.jpg"
+        }, {
+            name: "Ice Tea Watermelon",
+            price: 13,
+            img: "./imgs/iceDrinks/iceTeaMango.jpg"
+        }, {
+            name: "Ice Tea Strawberry",
+            price: 13,
+            img: "./imgs/iceDrinks/iceTeaStrawberry.jpg"
+        }, {
+            name: "Ice Tea Mango",
+            price: 13,
+            img: "./imgs/iceDrinks/iceTeaBlueberry.jpg"
+        }
+    ],
+    fuzzDrinks: [
+        {
+            name: "Cola",
+            price: 5,
+            img: "./imgs/fuzzDrinks/cola.jpg"
+        }, {
+            name: "Fanta",
+            price: 5,
+            img: "./imgs/fuzzDrinks/fanta.jpg"
+        }, {
+            name: "Sprite",
+            price: 5,
+            img: "./imgs/fuzzDrinks/sprite.jpg"
+        }, {
+            name: "Soda",
+            price: 5,
+            img: "./imgs/fuzzDrinks/soda.jpg"
+        }, {
+            name: "Blue",
+            price: 8,
+            img: "./imgs/fuzzDrinks/blue.jpg"
+        }, {
+            name: "XL",
+            price: 8,
+            img: "./imgs/fuzzDrinks/xl.jpg"
+        }, {
+            name: "Water",
+            price: 15,
+            img: "./imgs/fuzzDrinks/water.jpg"
+        }
+    ],
+    mojito: [
+        {
+            name: "Lemon & Mint",
+            price: 16,
+            img: "./imgs/mojito/lemonMint.jpg"
+        }, {
+            name: "Strawberry",
+            price: 16,
+            img: "./imgs/mojito/strawberry.jpg"
+        }, {
+            name: "Strawberry",
+            price: 16,
+            img: "./imgs/mojito/mango.jpg"
+        }, {
+            name: "Passtionfruit",
+            price: 16,
+            img: "./imgs/mojito/passionfruit.jpg"
+        }, {
+            name: "Mixed Berries",
+            price: 16,
+            img: "./imgs/mojito/mixedBerries.jpg"
+        }, {
+            name: "Candy",
+            price: 16,
+            img: "./imgs/mojito/candy.jpg"
+        }, {
+            name: "Watermelon",
+            price: 16,
+            img: "./imgs/mojito/watermelon.jpg"
+        }, {
+            name: "Mango",
+            price: 16,
+            img: "./imgs/mojito/mango.jpg"
+        }, {
+            name: "Piñacolada",
+            price: 16,
+            img: "./imgs/mojito/pinacolada.jpg"
+        }
+    ],
+    hotDrinks: [
+        {
+            name: "Tea",
+            price: 5,
+            img: "./imgs/hotDrinks/tea.jpg"
+        }, {
+            name: "Turkish Coffee",
+            price: 5,
+            img: "./imgs/hotDrinks/turkishCoffee.jpg"
+        }, {
+            name: "Nescafé",
+            price: 7,
+            img: "./imgs/hotDrinks/nescafe.jpg"
+        }, {
+            name: "Nescafé with Milk",
+            price: 10,
+            img: "./imgs/hotDrinks/nescafeWithMilk.jpg"
+        }, {
+            name: "Espresso",
+            price: "5/8",
+            img: "./imgs/hotDrinks/espresso.jpg"
+        }, {
+            name: "Cappuccino",
+            price: 7,
+            img: "./imgs/hotDrinks/cappuccino.jpg"
+        }, {
+            name: "French Vanilla",
+            price: 10,
+            img: "./imgs/hotDrinks/frenchVanilla.jpg"
+        }, {
+            name: "Hot Nutella",
+            price: 10,
+            img: "./imgs/hotDrinks/hotNutella.jpg"
+        }, {
+            name: "Hot Chocolate",
+            price: 10,
+            img: "./imgs/hotDrinks/hotChocolate.jpg"
+        }, {
+            name: "Hot Lotus",
+            price: 10,
+            img: "./imgs/hotDrinks/hotLotus.jpg"
+        }
+    ],
+    smoothies: [
+        {
+            name: "Mango",
+            price: 13,
+            img: "./imgs/smoothies/mango.jpg"
+        }, {
+            name: "Pineapple",
+            price: 13,
+            img: "./imgs/smoothies/pineapple.jpg"
+        }, {
+            name: "Strawberry",
+            price: 13,
+            img: "./imgs/smoothies/strawberry.jpg"
+        }, {
+            name: "Passionfruit",
+            price: 13,
+            img: "./imgs/smoothies/passionfruit.jpg"
+        }, {
+            name: "Lemon",
+            price: 13,
+            img: "./imgs/smoothies/lemon.jpg"
+        }, {
+            name: "Lemon & Mint",
+            price: 13,
+            img: "./imgs/smoothies/lemonMint.jpg"
+        }, {
+            name: "Mixed Berries",
+            price: 13,
+            img: "./imgs/smoothies/mixedBerries.jpg"
+        }, {
+            name: "Pomegranate",
+            price: 13,
+            img: "./imgs/smoothies/pomegranate.jpg"
+        }, {
+            name: "Mix 2-3 Fruits",
+            price: 13,
+            img: "./imgs/smoothies/mixFruits.jpg"
+        }
+    ],
+};
+// no description menu items
+// load menu items to html
+const loadMenu = (sectionID, src, h3, price) => {
+    const div = document.createElement("div");
+    div.className = "menu-item";
+    div.innerHTML = `
+        <img src="${src}" alt="" />
+        <h3>${h3}<br><span></span></h3>
+        <span class="price">${price}</span>`;
+
+    document.getElementById(sectionID).appendChild(div);
+};
+
+for (let i = 0; i < menu.hooka.length; i++) {
+    loadMenu("hooka", menu.hooka[i].img, menu.hooka[i].name, menu.hooka[i].price + " ₪");
+}
+for (let i = 0; i < menu.dessert.length; i++) {
+    loadMenu("dessert", menu.dessert[i].img, menu.dessert[i].name, menu.dessert[i].price + " ₪");
+}
+for (let i = 0; i < menu.iceCream.length; i++) {
+    loadMenu("iceCreem", menu.iceCream[i].img, menu.iceCream[i].name, menu.iceCream[i].price + " ₪");
+}
+for (let i = 0; i < menu.milkshake.length; i++) {
+    loadMenu("milkshake", menu.milkshake[i].img, menu.milkshake[i].name, menu.milkshake[i].price + " ₪");
+}
+for (let i = 0; i < menu.iceDrinks.length; i++) {
+    loadMenu("iceDrinks", menu.iceDrinks[i].img, menu.iceDrinks[i].name, menu.iceDrinks[i].price + " ₪");
+}
+for (let i = 0; i < menu.fuzzDrinks.length; i++) {
+    loadMenu("fuzzDrinks", menu.fuzzDrinks[i].img, menu.fuzzDrinks[i].name, menu.fuzzDrinks[i].price + " ₪");
+}
+for (let i = 0; i < menu.mojito.length; i++) {
+    loadMenu("mojito", menu.mojito[i].img, menu.mojito[i].name, menu.mojito[i].price + " ₪");
+}
+for (let i = 0; i < menu.hotDrinks.length; i++) {
+    loadMenu("hotDrinks", menu.hotDrinks[i].img, menu.hotDrinks[i].name, menu.hotDrinks[i].price + " ₪");
+}
+for (let i = 0; i < menu.smoothies.length; i++) {
+    loadMenu("freshJuice", menu.smoothies[i].img, menu.smoothies[i].name, menu.smoothies[i].price + " ₪");
+}
+// end load menu items to html
+
+// active menu link on scroll using observer api
+const sections = document.querySelectorAll("section");
+        const links = document.querySelectorAll(".nav-link");
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    // Formula: Match link whose href matches section id
+                    links.forEach(link => 
+                        link.classList.toggle("active", link.getAttribute("href").substring(1) === entry.target.id)
+                    );
+                }
+            });
+        }, { threshold: 0.20 }); // 40% in view = considered active
+        sections.forEach(section => observer.observe(section));
